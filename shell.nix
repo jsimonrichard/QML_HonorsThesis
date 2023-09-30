2060da92a18c4c72f5a90407ca725a367bfdfc5e
J. Simon Richard <jsimonrichard@gmail.com>
2023-09-30 16:54:08 -0400

    Followed along with qiskit QML course

--- shell.nix
+++ shell.nix
@@ -3,10 +3,13 @@ nixpkgs.mkShell {
   nativeBuildInputs = with nixpkgs; [
     poetry
     python310
+    pkgconfig
   ];
 
   LD_LIBRARY_PATH = builtins.concatStringsSep ":" [
     "${nixpkgs.stdenv.cc.cc.lib}/lib"
     "${nixpkgs.zlib.outPath}/lib"
   ];
+
+  # LLVM_CONFIG = "${nixpkgs.llvmPackages_10.llvm}/bin/llvm-config";
 }
